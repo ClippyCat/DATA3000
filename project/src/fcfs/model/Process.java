0c6df6250227752d488cabbf070c50e982dbf05e
bhavitha87 <86643898+bhavitha87@users.noreply.github.com>
2026-08-11 16:54:49 -0600

    Update Process.java

--- project/src/fcfs/model/Process.java
+++ project/src/fcfs/model/Process.java
@@ -1,26 +1,32 @@
 package fcfs.model;
 
-/**
- * Represents a process used in the FCFS scheduling algorithm.
- */
 public class Process {
 
-    private String processId;
+    // Basic process information
+    private String processID;
     private int arrivalTime;
     private int burstTime;
 
-      
-    /* Creates a process using its basic scheduling information.*/
-       
-     
+    // Scheduling information
+    private int startTime;
+    private int completionTime;
+    private int waitingTime;
+    private int turnaroundTime;
+
     public Process(String processId, int arrivalTime, int burstTime) {
+
         setProcessId(processId);
         setArrivalTime(arrivalTime);
         setBurstTime(burstTime);
+
+        this.startTime = 0;
+        this.completionTime = 0;
+        this.waitingTime = 0;
+        this.turnaroundTime = 0;
     }
 
-    public String getProcessId() {
-        return processId;
+    public String getProcessID() {
+        return processID;
     }
 
     public void setProcessId(String processId) {
@@ -30,7 +36,7 @@ public class Process {
             );
         }
 
-        this.processId = processId;
+        this.processID = processId;
     }
 
     public int getArrivalTime() {
@@ -61,10 +67,53 @@ public class Process {
         this.burstTime = burstTime;
     }
 
-  
-        @Override
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getCompletionTime() {
+        return completionTime;
+    }
+
+    public void setCompletionTime(int completionTime) {
+        this.completionTime = completionTime;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+	public int getTurnaroundTime() {
+        return turnaroundTime;
+    }
+
+    public void setTurnaroundTime(int turnaroundTime) {
+        this.turnaroundTime = turnaroundTime;
+    }
+
+    @Override
     public String toString() {
-        return processID + " Arrival=" + arrivalTime +
-               " Burst=" + burstTime;
+        return processID
+                + "\t"
+                + arrivalTime
+                + "\t"
+                + burstTime
+                + "\t"
+                + startTime
+                + "\t"
+                + completionTime
+                + "\t"
+                + waitingTime
+                + "\t"
+                + turnaroundTime;
     }
 }
+
